6d7b362c1e78a1d3cc575a5532a59e5be44a367e
thealacasse <93718457+thealacasse@users.noreply.github.com>
2023-04-16 13:27:11 -0400

    smiley

--- script/script.js
+++ script/script.js
@@ -1,3 +1,26 @@
-gsap.to(".icon-scroll", {
-  x: 200,
+let lastKnownScrollPosition = 0;
+let ticking = false;
+
+function minuteur(scrollPos) {
+  window.setTimeout(minuteur, 100);
+
+  myVar = setTimeout(minuteur, 100);
+  clearTimeout(myVar);
+
+  setTimeout(function (minuteur) {
+    $("#square").removeClass("is-scrolling");
+  }, 100);
+}
+
+document.addEventListener("scroll", (event) => {
+  lastKnownScrollPosition = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      minuteur(lastKnownScrollPosition);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
 });
